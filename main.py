384db94d7f9cb4a1cb35ab719fdae396f272f86c
islamatiyat01 <islamatiyat50@yahoo.com>
2022-10-01 22:14:16 +0300

    taskk

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello")
+
+length_1 = int(input('Enter your length: '))
+print(length_1)
+
+if length_1 > 170 or length_1 == 170:
+   print("tall")
+
+if length_1 < 170 and length_1 > 160:
+    print("normal")
+    
+if length_1 < 150:
+    print("short")
+
+
