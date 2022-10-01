7d7ef23e0e1929d23a8dac499adb48780605a1fe
islamatiyat01 <islamatiyat50@yahoo.com>
2022-10-01 22:27:46 +0300

    taskk

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 
+year = int(input('Enter  year: '))
+
+if (year % 400 == 0) and (year % 100 == 0):
+    print('the is leap year')
+
+elif (year % 4 == 0) and (year % 100 == 0):
+    print('the year is a leap year')
+else:
+    print(" is not a leap year")
+
+    print("************************")
+
 length_1 = int(input('Enter your length: '))
 print(length_1)
 
@@ -7,7 +19,7 @@ if length_1 > 170 or length_1 == 170:
 
 if length_1 < 170 and length_1 > 160:
     print("normal")
-    
+
 if length_1 < 150:
     print("short")
 
